a75862db85811fbfb45d0dfd8ab7d0b514a57200
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-12-19 13:40:49 +0200

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
     this._sqft = sqft;
   }
 
+  // sqft getter
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
   }
 }
