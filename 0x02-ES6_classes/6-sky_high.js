01862ee03c6c4a6459bb207ad7dc92e69f0e139b
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-12-19 13:25:43 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,25 +1,7 @@
-// 5-building.js
-export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must implement evacuationWarningMessage');
-    }
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    return 'Emergency! Evacuate the building!';
-  }
-}
-
 // 6-sky_high.js
 import Building from './5-building.js';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
@@ -33,3 +15,5 @@ export default class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+export default SkyHighBuilding;
