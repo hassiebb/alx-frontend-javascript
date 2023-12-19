90bdfc4f669233063c58933b7ad59939fca02bfe
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-12-19 13:22:21 +0200

    Update 6-main.js

--- 0x02-ES6_classes/6-main.js
+++ 0x02-ES6_classes/6-main.js
@@ -1,6 +1,7 @@
-import SkyHighBuilding from './6-sky_high';
+// 6-main.js
+import SkyHighBuilding from './6-sky_high.js';
 
 const building = new SkyHighBuilding(140, 60);
-console.log(building.sqft);
-console.log(building.floors);
-console.log(building.evacuationWarningMessage());
+console.log(building.sqft); // Output: 140
+console.log(building.floors); // Output: 60
+console.log(building.evacuationWarningMessage()); // Output: Evacuate slowly the 60 floors
